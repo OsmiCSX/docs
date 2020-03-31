4401388509a7e02d21e2fd45f05987e626f5ecc7
Ryan Aunur <ryandevstudio@gmail.com>
2020-03-31 16:26:11 +0700

    update: add small logo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
   head: headers,
   base: '/docs/',
   themeConfig: {
-    logo: '/logo.png',
+    logo: '/logo-small.png',
     domain: process.env.CFG_DOMAIN,
     displayAllHeaders: true,
     nav: [{
